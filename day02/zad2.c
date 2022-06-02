aa9e1057957ed6a5f67746f1d297fe0000a105c3
vsokolov <v11sokolov@gmail.com>
2022-06-02 16:26:35 +0300

    homework

--- day02/zad2.c
+++ day02/zad2.c
@@ -1,20 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int xor(int a, int b){
-    //ako dvete sa razlichni znachi zaduljitelno ednoto e =1 a drugoto =0
-    if(a != b){
-        return 0;
+    //ako se vuvejdat samo 2 chisla programata moje da raboti i  s if(a != b){return true}
+    if(!a && b ){
+        return 1;
+    }
+    else if(a  && !b){
+        return 1;
     }
     else{
-        return 1;
+        return 0;
     }
 }
 int main(){
-
     int a,b;
-    printf("Enter A(0 or 1):");
+    printf("Enter A(0 or 1): ");
     scanf("%d", &a);
-    printf("Enter B(0 or 1):");
-    scanf("%d", &a);
-    printf("%d\n", xor(a, b));
+    printf("\nEnter B(0 or 1): ");
+    scanf("%d", &b);
+    printf("%d\n", xor(a,b));
 }
+//.zad2 < numbers.txt
