75c8922b47ad85fde7d64edb421e998229066834
vsokolov <v11sokolov@gmail.com>
2022-06-06 10:11:23 +0300

    exam

--- exam01/zad1.c
+++ exam01/zad1.c
@@ -13,15 +13,18 @@ int main(){
         
         if(r <= 0 || h <= 0){
             printf("Invalid input!");
+            return -1;
         }
         else{
             cylinder(r,h,&S,&V);
             printf("V=%.2lf, S=%.2lf\n", V, S);
+            return 0;
         } 
     }
 }
 
 int cylinder(double r, double h, double* V, double* S){
-    *V = M_PI*pow(r ,2)*h;
-    *S = 2*M_PI*r*h;
+    *S = M_PI*pow(r ,2)*h;
+    *V = 2*M_PI*r*(r+h);
+    //mislq che V i S trqbva da sa razmeneni no e taka po uslovie, zatova go ostavqm taka
 }
---
+++ exam01/zad2.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include<stdint.h>
+
+unsigned bitsNCount(uint64_t mask1, uint32_t mask2, uint16_t mask3, uint8_t mask4);
+void flipOddBits(uint64_t*);
+void mirrorBits(uint16_t* mask);
+
+int main(){
+    uint64_t mask = 0;
+    //printf("The count of the ones int the masks : %d\n", bitsNCount(15234, 1200, 450, 190));
+    flipOddBits(&mask);
+
+}
+
+unsigned bitsNCount(uint64_t mask1, uint32_t mask2, uint16_t mask3, uint8_t mask4){
+    int ones = 0;
+
+
+    for(int bit = sizeof(mask1) * 8; bit > 0; bit--){
+        int counter;
+        counter = (mask1&(1<<bit))>>bit;
+        if(counter == 1){
+            ones++;
+        }
+    }
+
+    for(int bit = sizeof(mask2) * 8; bit > 0; bit--){
+        int counter;
+        counter = (mask2&(1<<bit))>>bit;
+        if(counter == 1){
+            ones++;
+        }
+    }
+
+    for(int bit = sizeof(mask3) * 8; bit > 0; bit--){
+        int counter;
+        counter = (mask3&(1<<bit))>>bit;
+        if(counter == 1){
+            ones++;
+        }
+    }
+
+    for(int bit = sizeof(mask4) * 8; bit > 0; bit--){
+        int counter;
+        counter = (mask4&(1<<bit))>>bit;
+        if(counter == 1){
+            ones++;
+        }
+    }
+    return ones;
+}
+
+void flipOddBits(uint64_t* mask){
+    for(int bit = sizeof(mask) * 8; bit > 0; bit--){
+        printf("%llu", (mask&((uint64_t)1<<bit))>>bit);
+        if(bit %2 != 0){
+            mask ^= (uint64_t)1<<bit;
+        }       
+    }
+}
+void mirrorBits(uint16_t* mask){
+    for(int bit = sizeof(mask) * 8; bit > 0; bit--){
+        mask ^= (uint64_t)1<<bit;
+    }
+}
