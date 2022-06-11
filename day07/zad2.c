409c3639454f30b740eee2beded8c9a6c9530fde
vsokolov <v11sokolov@gmail.com>
2022-06-11 10:33:22 +0300

    hw

---
+++ day07/zad2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int sumArr(const int*,int);
+
+int main(){
+    int arr[5] = {10,3,6,12,7};
+    printf("%d\n", sumArr(arr, 5));
+}
+int sumArr(const int* arr, int size){
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        sum+=arr[i];
+    }
+    return sum;
+}
